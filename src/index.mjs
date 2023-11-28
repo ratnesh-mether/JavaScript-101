76035ce718620cae228c73de750d63e6afb7884d
Ratnesh Mether <139987154+ratnesh-mether@users.noreply.github.com>
2023-11-28 00:10:05 +0000

    [6] : Map-Filter-Reduce

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,8 @@
 import "./styles.css";
 // ------------CLOSURES--------------
-import "./core-concepts/closures.mjs";
+// import "./core-concepts/closures.mjs";
+
+//------------Map-Filter-Reduce--------------
+
+import "./core-concepts/map-filter-reduce.mjs";
+
