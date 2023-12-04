03084c4402c436c46f2ed89297fd77a4fe11b569
Ratnesh Mether <139987154+ratnesh-mether@users.noreply.github.com>
2023-12-04 17:32:35 +0000

    [17] : Mock Practice.

--- src/output-based-questions/predict-output.js
+++ src/output-based-questions/predict-output.js
@@ -1,9 +1,20 @@
-var obj = {
-  name: "vivek",
-  getName: function () {
-    console.log(this);
-    console.log(this.name);
-  },
-};
+// var obj = {
+//   name: "vivek",
+//   getName: function () {
+//     console.log(this);
+//     console.log(this.name);
+//   },
+// };
 
-obj.getName();
+// obj.getName();
+
+function func1() {
+  setTimeout(() => {
+    console.log(x);
+    console.log(y);
+  }, 3000);
+
+  var x = 2;
+  let y = 12;
+}
+func1();
