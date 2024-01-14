17bcefecf989ad963ee42c36265d52523735faa9
Ratnesh Mether <139987154+ratnesh-mether@users.noreply.github.com>
2024-01-14 12:57:48 +0000

    [27] : Completed D & T

--- src/core-concepts/debounce-throttle-v1.mjs
+++ src/core-concepts/debounce-throttle-v1.mjs
@@ -1,11 +1,46 @@
 let counter = 0;
 let debouncedCounter = 0;
+let throttledCounter = 0;
+let flag = true;
 const button = document.getElementById("button");
 const headingOne = document.getElementById("heading-one");
+const headingTwo = document.getElementById("heading-two");
+const headingThree = document.getElementById("heading-three");
 
-button.addEventListener("click", incrementCount);
-headingOne.innerHTML = `COUNTER : ${counter}`;
-const incrementCount = () => {
-  counter++;
+const myDebounce = (callback, delay) => {
+  let timer;
+  return function () {
+    if (timer) clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      callback();
+    }, delay);
+  };
 };
-console.log(button);
+
+const myThrottle = (callback, delay) => {
+  return function () {
+    if (flag) {
+      flag = false;
+      setTimeout(() => {
+        callback();
+      }, delay);
+    }
+  };
+};
+const debounce = myDebounce(() => {
+  debouncedCounter += 1;
+  headingTwo.innerHTML = `DEBOUNCE-COUNTER : ${debouncedCounter}`;
+}, 1000);
+const throttle = myThrottle(() => {
+  throttledCounter++;
+  flag = true;
+  headingThree.innerHTML = `THROTTLE ${throttledCounter}`;
+}, 1000);
+const incrementCount = () => {
+  counter += 1;
+  headingOne.innerHTML = `COUNTER : ${counter}`;
+  // debounce();
+  throttle();
+};
+button.addEventListener("click", incrementCount);
