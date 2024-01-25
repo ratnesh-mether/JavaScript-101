7204ec01955084fcb3c5b48b54e99fe3a3d9cec5
Ratnesh Mether <139987154+ratnesh-mether@users.noreply.github.com>
2024-01-25 13:21:50 +0000

    [39] : Reverse String

---
+++ src/coding-problems/reverse-string.mjs
@@ -0,0 +1,12 @@
+let str = "abcd";
+
+/* console.log(str.substr(1))
+ */
+/* const cleanedStr = str.replace(/[^\w]/g, '');
+console.log(cleanedStr.length) */
+
+function reversString(str) {
+  return str === "" ? "" : reversString(str.substr(1)) + str.charAt(0);
+}
+
+console.log(reversString(str));
